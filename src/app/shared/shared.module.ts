00eb1663a507d5ffbd0b44ac99b3e6e1560bb9bb
Peter Burjan <peter.burjan@rbi.co.uk>
2017-05-20 12:23:31 +0100

    Added core module, pre loading lazy module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from "@angular/core";
 import {DropdownDirective} from "./dropdown.directive";
 import {CommonModule} from "@angular/common";
 
+// Best not to provide services in shared modules, it will use a child injector not a root injector!
 @NgModule({
   declarations: [
     DropdownDirective
